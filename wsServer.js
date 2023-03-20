94d3e09866bdc4a32d76e48629bf66cb319a72a7
HOANGLINHONLINE\hienlk <killer0121906@gmail.com>
2023-03-20 17:48:22 +0700

    fix some thing

--- wsServer.js
+++ wsServer.js
@@ -1,18 +1,19 @@
 const websocket = require("ws");
 const net = require("net");
-const { v4: uuidv4 } = require("uuid");
+const short = require("short-uuid");
 
 //-------------------------//////////////////////////
 //websocketServer
 const wss_server = new websocket.WebSocketServer({ port: 8080 });
-let clients = [];
-let room = [];
+
+let wss_clients = [];
+let wss_rooms = [];
 
 console.log(" server ws run at port : ", wss_server.address().port);
 wss_server.on("connection", (ws) => {
   ws.room = [];
-  ws.id = uuidv4();
-  clients.push(ws);
+  ws.id = short.generate();
+  wss_clients.push(ws);
   ws.send("your ID : \n " + ws.id);
   ws.on("message", (message) => {
     messag = JSON.parse(message);
@@ -20,17 +21,17 @@ wss_server.on("connection", (ws) => {
     if (messag.chatRoom) {
       let message = messag.message;
       let room = messag.room;
-      chatRoom(ws, room, message);
+      chatRoom(room, message);
     }
     if (messag.chat1vs1) {
       let message = messag.message;
       let des = messag.des;
-      chat1vs1(ws, des, message);
+      chat1vs1(des, message);
     }
     if (messag.joinRoom) {
       let roomId = messag.joinRoom;
       ws.room.push(roomId);
-      if (room.indexOf(roomId)) room.push(roomId);
+      if (wss_rooms.indexOf(roomId)) wss_rooms.push(roomId);
     }
     if (messag.leaveRoom) {
       var index = ws.room.indexOf(item);
@@ -51,10 +52,14 @@ wss_server.on("connection", (ws) => {
 ////-----------------------------------------///
 // net Server
 
+let net_clients = [];
+let net_rooms = [];
+
 var Net_server = net.createServer(function (ws) {
   ws.room = [];
-  ws.id = uuidv4();
-  clients.push(ws);
+  ws.id = short.generate();
+  net_clients.push(ws);
+
   ws.write("your ID : \n " + ws.id);
   ws.on("data", (message) => {
     messag = JSON.parse(message);
@@ -63,20 +68,20 @@ var Net_server = net.createServer(function (ws) {
     if (messag.chatRoom) {
       let message = messag.message;
       let room = messag.room;
-      chatRoom(ws, room, message);
+      chatRoom(room, message);
     }
     if (messag.chat1vs1) {
       let message = messag.message;
       let des = messag.des;
-      chat1vs1(ws, des, message);
+      chat1vs1(des, message);
     }
     if (messag.joinRoom) {
       let roomId = messag.joinRoom;
-      ws.room.push(roomId);
-      if (room.indexOf(roomId)) room.push(roomId);
+      net_rooms.push(roomId);
+      if (!room.indexOf(roomId)) room.push(roomId);
     }
     if (messag.leaveRoom) {
-      var index = ws.room.indexOf(item);
+      var index = ws.room.indexOf(message.leaveRoom);
       if (index !== -1) {
         ws.room.splice(index, 1);
       }
@@ -101,23 +106,34 @@ setInterval(() => {
 
 function WorldClock() {
   wss_server.clients.forEach((client) => {
-    client.send(new Date().toISOString());
+    client.send(new Date().toISOString() + "\n");
   });
 
-  Net_server.getConnections().forEach((client) => {
-    client.write(new Date().toISOString());
+  net_clients.forEach((client) => {
+    client.write(new Date().toISOString() + "\n");
   });
 }
 
-function chatRoom(ws, room, message) {
-  wss_server.clients.forEach((client) => {
+function chatRoom(room, message) {
+  wss_clients.forEach((client) => {
     if (client.room.indexOf(room) > -1) {
       client.send(`from ${room} user ${ws.id}:   ${message}`);
     }
   });
+
+  net_clients.forEach((client) => {
+    if (client.room.indexOf(room) > -1) {
+      client.send(` from  ${ws.id} :   ${message}`);
+    }
+  });
 }
-function chat1vs1(ws, destination, message) {
-  wss_server.clients.forEach((client) => {
+function chat1vs1(destination, message) {
+  wss_clients.forEach((client) => {
+    if (client.id == destination) {
+      client.send(` from  ${ws.id} :   ${message}`);
+    }
+  });
+  net_clients.forEach((client) => {
     if (client.id == destination) {
       client.send(` from  ${ws.id} :   ${message}`);
     }
@@ -130,8 +146,8 @@ function getlistIDuser() {
     result.push(client.id);
   });
 
-  Net_server.clients.forEach((client) => {
-    result.push(client.id);
+  Net_server.getConnections((err, count) => {
+    console.log(count);
   });
 
   return result;
